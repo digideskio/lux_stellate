0f8e32d4d25bd19d671514fc91f1b3daac6be009
Josh Lind <jlind@tableausoftware.com>
2014-06-14 21:43:04 -0700

    3d shape light functions

--- lux_stellate.ino
+++ lux_stellate.ino
@@ -15,6 +15,7 @@ PololuLedStrip<5> ledStrip2;
 rgb_color meta_strip[665];
 
 static unsigned int wait = 50;
+static unsigned int segment_length = LED_COUNT_PER_STRIP / 6;
 
 /**
  * Required functions...
@@ -41,7 +42,7 @@ void loop() {
  * Light patterns...
  */
 
-int rainbow() {
+void rainbow() {
   byte time = millis() >> 2;
   for(uint16_t i = 0; i < LED_COUNT_PER_STRIP; i++) {
     byte x = time - 8*i;
@@ -52,10 +53,70 @@ int rainbow() {
   ledStrip2.write(meta_strip, LED_COUNT_PER_STRIP);
 }
 
+void segment_oscillate() {
+  for (int i=0; i<6; i++) {
+    light_segment(i, 0, 150);
+  }
+}
+
+void corner_oscillate() {
+  uint32_t color = Wheel(c);
+  for (int i=0; i<4; i++) {
+    switch (i) {
+      case 1:
+        light_segment(1, 1, color);
+        light_segment(3, 2, color);
+        light_segment(4, 1, color);
+        break;
+      case 2:
+        light_segment(1, 2, color);
+        light_segment(2, 1, color);
+        light_segment(5, 2, color);
+        break;
+      case 3:
+        light_segment(2, 2, color);
+        light_segment(3, 1, color);
+        light_segment(6, 1, color);
+        break;
+      case 4:
+        light_segment(4, 2, color);
+        light_segment(5, 1, color);
+        light_segment(6, 2, color);
+        break;
+    }
+  }
+}
+
 
 /**
  * Helper functions...
  */
+
+/**
+ * Illumiate a portion of the edge of the tetrahedron.
+ */
+int light_segment(int index, int half, int c) {
+  uint32_t color = Wheel(c);
+  int start = segment_length * (index -1)
+
+  for (int i=start; i <= segment_length+start; i++) {
+    if (i < (segment_length/2)) {
+       // First half.
+      if (half == 0 || half == 1) {
+        meta_strip[i] = color;
+      }
+    }
+    else {
+      // Second half.
+      if (half == 0 || half == 2) {
+        meta_strip[i] = color;
+      }
+    }
+  }
+
+  ledStrip1.write(meta_strip, LED_COUNT_PER_STRIP);
+  ledStrip2.write(meta_strip, LED_COUNT_PER_STRIP);
+}
 
 /**
  * Create a 24 bit color value from R,G,B.
